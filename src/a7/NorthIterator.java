dacbc316795e379350179f3f0420fe8354cfcd4e
justinbatchelor <jrbatch@live.unc.edu>
2019-11-19 22:41:01 -0500

    Need to figure out how to skip a players turn if there are no available moves... Other than that Othello is complete...

---
+++ src/a7/NorthIterator.java
@@ -0,0 +1,29 @@
+package a7;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class NorthIterator implements Iterator<Spot> {
+
+    private int _currentX;
+    private int _currentY;
+    private SpotBoard _board;
+
+    public NorthIterator(SpotBoard board, int startX, int startY) {
+        _board = board;
+        _currentX = startX;
+        _currentY = startY;
+    }
+    public boolean hasNext() {
+        return ((_currentY-1) >= 0);
+    }
+
+    public Spot next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        _currentY -=1;
+        Spot spot = _board.getSpotAt(_currentX, _currentY);
+        return spot;
+    }
+}
